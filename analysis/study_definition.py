12cea5eb72250436ef81d508d1872c6a470565f0
jg <john.b.gavin+github@gmail.com>
2023-08-24 19:02:13 +0000

    docs.opensafely.org/getting-started /#add-an-age-column

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -8,6 +8,17 @@ study = StudyDefinition(
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
+        # all patients who have never changed practice, between these two dates
         "2019-02-01", "2020-02-01"
+    ),
+    age=patients.age_as_of(
+        # column of data corresponding to the age of each patient on the given date
+        "2019-09-01",
+        return_expectations={
+            # every patient to have a value,
+            # and the distribution of ages to match that of the real UK population
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
     ),
 )
